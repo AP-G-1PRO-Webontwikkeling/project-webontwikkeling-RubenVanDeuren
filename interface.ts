f69dbe6fd764aa9f8492539382512b776e91f17e
ruben van deuren <Rubenvd04@gmail.com>
2024-02-29 15:18:41 +0000

    deel2

---
+++ interface.ts
@@ -0,0 +1,12 @@
+interface commander{
+    id:string,
+    name: string,
+    oracle:string,
+    cmc: number,
+    standardLegal:boolean,
+    releasedate:string,
+    image:string,
+    rarity:string,
+    colorIdentity:string[],
+    guild:JSON
+}
