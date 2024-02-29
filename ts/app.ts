ced5aa180287ef7be69b922df443eb08c602b961
ruben van deuren <Rubenvd04@gmail.com>
2024-02-29 16:03:10 +0000

    fix

---
+++ ts/app.ts
@@ -0,0 +1,12 @@
+import * as readline from 'readline-sync';
+
+function app(){    
+    console.log("Welcome to the JSON data viewer!")
+    console.log()
+    console.log("1. View all data")
+    console.log("2. Filter by ID")
+    console.log("3. Exit")
+    console.log()
+}
+
+app();
